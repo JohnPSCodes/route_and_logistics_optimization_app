a11207273267c3a67292a98f3a38148ce786eccd
JohnPSCodes <rapidostutoriales117@gmail.com>
2025-08-14 09:18:16 -0400

    working on the last two models of the MVP (Routes & Stops)

--- route_optimization/apps/backend/urls.py
+++ route_optimization/apps/backend/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet,OrderViewSet
+from .views import UserViewSet,OrderViewSet,RouteViewSet,StopViewSet
 
 router = DefaultRouter()
 router.register(r'users',UserViewSet)
 router.register(r'orders',OrderViewSet)
+router.register(r'routes',RouteViewSet)
+router.register(r'stops',StopViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
--- route_optimization/apps/backend/views.py
+++ route_optimization/apps/backend/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework import viewsets
-from .models import User,Order
-from .serializers import UserSerializer,OrderSerializer
+from .models import User,Order,Route,Stop
+from .serializers import UserSerializer,OrderSerializer,RouteSerializer,StopSerializer
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+class StopViewSet(viewsets.ModelViewSet):
+    queryset = Stop.objects.all()
+    serializer_class = StopSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
