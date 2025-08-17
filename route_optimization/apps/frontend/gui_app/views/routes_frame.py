95dfe2dc8c3342c1c0395f8355a494e472a2d0a4
JohnPSCodes <rapidostutoriales117@gmail.com>
2025-08-17 19:28:00 -0400

    frontend update: added routes frame

---
+++ route_optimization/apps/frontend/gui_app/views/routes_frame.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from tkinter import ttk
+
+class RoutesFrame(tk.Frame):
+    def __init__(self,parent,controller):
+        super().__init__(parent,bg="white")
+        tk.Label(self,text="Routes management",font=("Arial",18),bg="white").pack(pady=20)
