e215e23684c0c5e40cfd3f58509c5694eb5f546b
JohnPSCodes <rapidostutoriales117@gmail.com>
2025-08-29 23:35:04 -0400

    added new urls to router

--- route_optimization/apps/backend/urls.py
+++ route_optimization/apps/backend/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet,OrderViewSet,RouteViewSet,StopViewSet
+from .views import UserViewSet,OrderViewSet,RouteViewSet,StopViewSet,DriverViewSet,RouteInfoViewSet
 
 router = DefaultRouter()
 router.register(r'users',UserViewSet)
 router.register(r'orders',OrderViewSet)
 router.register(r'routes',RouteViewSet)
 router.register(r'stops',StopViewSet)
+router.register(r'drivers', DriverViewSet)
+router.register(r'route_info',RouteInfoViewSet)
 
 urlpatterns = [
     path('',include(router.urls)),
