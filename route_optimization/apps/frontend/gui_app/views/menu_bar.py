8c9abb307e371d3afabb015616ffe57a8a0d19d3
JohnPSCodes <rapidostutoriales117@gmail.com>
2025-08-29 23:29:25 -0400

    Update with some placeholders

--- route_optimization/apps/frontend/gui_app/views/menu_bar.py
+++ route_optimization/apps/frontend/gui_app/views/menu_bar.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+from apps.frontend.gui_app.utils.calculator import Calculator  # Importamos la calculadora
 
 class MenuBar:
     def __init__(self, master):
@@ -9,37 +10,33 @@ class MenuBar:
     def create_menu(self):
         menu_bar = tk.Menu(self.master)
 
-        # --------- Files Menu ---------
+        # Files Menu
         files_menu = tk.Menu(menu_bar, tearoff=0)
         files_menu.add_command(label="New", accelerator="Ctrl+N", command=self.new_file)
         files_menu.add_command(label="Open", accelerator="Ctrl+O", command=self.open_file)
         files_menu.add_separator()
         files_menu.add_command(label="Exit", accelerator="Ctrl+Q", command=self.exit_app)
 
-        # --------- Help Menu ---------
+        # Utilities Menu
+        utilities_menu = tk.Menu(menu_bar, tearoff=0)
+        utilities_menu.add_command(label="Calculator", command=lambda: Calculator(self.master))
+        utilities_menu.add_command(label="Unit Converter", command=self.unit_converter)
+
+        # Help Menu
         help_menu = tk.Menu(menu_bar, tearoff=0)
         help_menu.add_command(label="Help", command=self.show_help)
         help_menu.add_command(label="About", command=self.show_about)
 
-        # --------- Config Menu ---------
+        # Config Menu
         config_menu = tk.Menu(menu_bar, tearoff=0)
         config_menu.add_command(label="Option 1", command=lambda: self.show_option("Option 1"))
-        config_menu.add_command(label="Option 2", command=lambda: self.show_option("Option 2"))
-        config_menu.add_command(label="Option 3", command=lambda: self.show_option("Option 3"))
-        config_menu.add_command(label="Option 4", command=lambda: self.show_option("Option 4"))
 
-        # Add menus to the menubar
         menu_bar.add_cascade(label="Files", menu=files_menu)
-        menu_bar.add_cascade(label="Help", menu=help_menu)
+        menu_bar.add_cascade(label="Utilities", menu=utilities_menu)
         menu_bar.add_cascade(label="Config", menu=config_menu)
+        menu_bar.add_cascade(label="Help", menu=help_menu)
 
-        # Attach menubar to the main window
         self.master.config(menu=menu_bar)
-
-        # Optional: bind accelerators
-        self.master.bind_all("<Control-n>", lambda e: self.new_file())
-        self.master.bind_all("<Control-o>", lambda e: self.open_file())
-        self.master.bind_all("<Control-q>", lambda e: self.exit_app())
 
     # --------- Placeholder methods ---------
     def new_file(self):
@@ -59,3 +56,6 @@ class MenuBar:
 
     def show_option(self, option_name):
         messagebox.showinfo(option_name, f"{option_name} selected.")
+
+    def unit_converter(self):
+        messagebox.showinfo("Unit Converter", "Coming soon!")
