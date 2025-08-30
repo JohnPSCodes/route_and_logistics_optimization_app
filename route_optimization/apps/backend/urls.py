1ec6796dc2941cb3379f010c2b75f8e8f49e0cd8
JohnPSCodes <rapidostutoriales117@gmail.com>
2025-08-30 09:18:53 -0400

    urls removed

--- route_optimization/apps/backend/urls.py
+++ route_optimization/apps/backend/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet,OrderViewSet,RouteViewSet,StopViewSet,DriverViewSet,RouteInfoViewSet
+from .views import UserViewSet,OrderViewSet,RouteViewSet,StopViewSet
 
 router = DefaultRouter()
 router.register(r'users',UserViewSet)
 router.register(r'orders',OrderViewSet)
 router.register(r'routes',RouteViewSet)
 router.register(r'stops',StopViewSet)
-router.register(r'drivers', DriverViewSet)
-router.register(r'route_info',RouteInfoViewSet)
+
 
 urlpatterns = [
     path('',include(router.urls)),
